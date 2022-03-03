f7123ffb1b0b769221759267702e10fd68c7f4fb
Emils Melders <emils.melders@datigroup.com>
2022-03-03 16:07:50 +0200

    Add message property parameter

--- cayennelpp-decoder-node.js
+++ cayennelpp-decoder-node.js
@@ -1,15 +1,18 @@
-var decoder = require('./decode.js');
+var decoder = require("./decode.js");
 
-module.exports = function(RED) {
-
+module.exports = function (RED) {
   function CayenneLPPDecoderNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
 
-    node.on('input', function(msg) {
-      msg.payload = decoder.decodeCayenneLpp(msg.payload);
+    node.on("input", function (msg) {
+      var value = RED.util.getMessageProperty(msg, node.property);
+      if (value !== undefined) {
+        value = decoder.decodeCayenneLpp(msg.payload);
+        RED.util.setMessageProperty(msg, node.property, value);
+      }
       node.send(msg);
     });
   }
   RED.nodes.registerType("cayennelpp-decoder", CayenneLPPDecoderNode);
-}
+};
